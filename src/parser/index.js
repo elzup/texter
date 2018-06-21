641e60f74b72928b90255b81bfdd9135ac0cf6e0
elzup <guild0105@gmail.com>
2018-06-21 20:44:57 +0900

    Parser comp

--- src/parser/index.js
+++ src/parser/index.js
@@ -1,9 +1,11 @@
 // @flow
 import type { ParseResult, Block } from '../types'
 
+const re = /\{(.*?)\}/
+const re2 = /\[(.*?)\]/
+
 function parseTexter(text: string): ParseResult {
 	const blocks: Block[] = [{ type: 'text', text }]
-	const re = /\{(.*?)\}/
 	let m = re.exec(text)
 	while (m) {
 		const block = blocks.pop()
@@ -16,6 +18,26 @@ function parseTexter(text: string): ParseResult {
 		blocks.push({ type: 'input', name })
 		blocks.push({ type: 'text', text: tail })
 		m = re.exec(tail)
+	}
+	let i = 0
+	while (blocks.length > i) {
+		const block = blocks[i]
+		if (block.type !== 'text') {
+			i += 1
+			continue
+		}
+		m = re2.exec(block.text)
+		if (!m) {
+			i += 1
+			continue
+		}
+		const [hit, names]: [string, string] = m
+		const [head, tail] = block.text.split(hit)
+		blocks.pop()
+		blocks.push({ type: 'text', text: head })
+		blocks.push({ type: 'select', texts: names.split('|') })
+		blocks.push({ type: 'text', text: tail })
+		i += 3
 	}
 	return { ok: true, blocks }
 }
