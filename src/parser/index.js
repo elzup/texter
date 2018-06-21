198e4029014f8e50021068949182f77e0c7cb093
elzup <guild0105@gmail.com>
2018-06-22 07:18:54 +0900

    Fix parser order

--- src/parser/index.js
+++ src/parser/index.js
@@ -1,44 +1,42 @@
 // @flow
 import type { ParseResult, Block } from '../types'
+import Extractor from 'extract-brackets'
 
 const re = /\{(.*?)\}/
 const re2 = /\[(.*?)\]/
 
 function parseTexter(text: string): ParseResult {
-	const blocks: Block[] = [{ type: 'text', text }]
+	const blocks1: Block[] = [{ type: 'text', text }]
 	let m = re.exec(text)
 	while (m) {
-		const block = blocks.pop()
+		const block = blocks1.pop()
 		if (block.type !== 'text') {
 			break
 		}
 		const [hit, name] = m
 		const [head, tail] = block.text.split(hit)
-		blocks.push({ type: 'text', text: head })
-		blocks.push({ type: 'input', name })
-		blocks.push({ type: 'text', text: tail })
+		blocks1.push({ type: 'text', text: head })
+		blocks1.push({ type: 'input', name })
+		blocks1.push({ type: 'text', text: tail })
 		m = re.exec(tail)
 	}
-	let i = 0
-	while (blocks.length > i) {
-		const block = blocks[i]
+	const blocks: Block[] = []
+	blocks1.forEach(block => {
 		if (block.type !== 'text') {
-			i += 1
-			continue
+			blocks.push(block)
+			return
 		}
 		m = re2.exec(block.text)
 		if (!m) {
-			i += 1
-			continue
+			blocks.push(block)
+			return
 		}
 		const [hit, names]: [string, string] = m
 		const [head, tail] = block.text.split(hit)
-		blocks.pop()
 		blocks.push({ type: 'text', text: head })
 		blocks.push({ type: 'select', texts: names.split('|') })
 		blocks.push({ type: 'text', text: tail })
-		i += 3
-	}
+	})
 	return { ok: true, blocks }
 }
 
--- src/parser/index.test.js
+++ src/parser/index.test.js
@@ -4,9 +4,5 @@ test('works', () => {
 	expect(m('kyoko kyoko toshino.')).toMatchSnapshot()
 	expect(m('私は[ゼニガメ|ヒトカゲ|フシギダネ]を選んだ')).toMatchSnapshot()
 	expect(m('my name is {name}.')).toMatchSnapshot()
-	expect(
-		m(
-			'【定期】{message1} {teamA} vs {teamB} 戦績{win}-{lose} {stage}({rule}){result} {message2}',
-		),
-	).toMatchSnapshot()
+	expect(m('[a|b]{a}')).toMatchSnapshot()
 })
