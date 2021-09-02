3a7db441bc131b5853f1943338c34b2864d5565b
elzup <elzup@users.noreply.github.com>
2021-09-02 00:34:22 +0000

    deploy: b5c46b3d6f1a715f1b5f938368af343d4147f6aa

---
+++ _next/static/rejkpXtK7vONvRyBr-9o6/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-a04946f0b3a6930a83e7.js"],"/_error":["static/chunks/pages/_error-eb7f79f2af83e8ddf5e2.js"],"/load":["static/chunks/pages/load-9cca8be7d53ec5795b97.js"],sortedPages:["/","/_app","/_error","/load"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
