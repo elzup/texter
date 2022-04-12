8ffb567577b01003b4d0bef72a95c1268fca9da8
elzup <elzup@users.noreply.github.com>
2022-04-12 03:25:45 +0000

    deploy: 801d8902d83c51ed6894e48b40def6fdd35e1530

---
+++ _next/static/MwYLoNP-wb3cvDEdjmcHj/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-920cc3c0adf8b2f7.js"],"/_error":["static/chunks/pages/_error-5b31083fea36d153.js"],"/load":["static/chunks/pages/load-0805cd21292f2de1.js"],sortedPages:["/","/_app","/_error","/load"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
